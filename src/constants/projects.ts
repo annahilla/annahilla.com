f258c79cddac4a172cacf4c598306e58a1a382cc
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-20 16:09:49 +0200

    Add more projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,9 @@
 import project1 from "../assets/projects/project1.png";
 import project2 from "../assets/projects/project2.png";
 import project3 from "../assets/projects/project3.png";
+import project4 from "../assets/projects/project4.png";
+import project5 from "../assets/projects/project5.png";
+import project6 from "../assets/projects/project6.png";
 
 export const projects = [
     {
@@ -20,5 +23,23 @@ export const projects = [
         name: "Wellpulse",
         src: project3.src,
         link: "https://wellpulse.vercel.app/"
+    },
+    {
+        id: 4,
+        name: "Budget Calculator",
+        src: project4.src,
+        link: "https://web-budget-calculator.vercel.app/"
+    },
+    {
+        id: 5,
+        name: "Digital Onboarding",
+        src: project5.src,
+        link: "https://digital-onboarding.vercel.app/"
+    },
+    {
+        id: 6,
+        name: "Expense Tracker",
+        src: project6.src,
+        link: "https://expense-tracker-sable-two.vercel.app/"
     },
 ]
