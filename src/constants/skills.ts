bacdb18965d21da9c70a8a18484f70899379faf7
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-20 22:42:00 +0200

    Classify skills

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,90 +1,40 @@
-export const skills = [
-  {
-    name: "JavaScript",
-    logo: "https://img.shields.io/badge/JavaScript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black"
-  },
-  {
-    name: "React",
-    logo: "https://img.shields.io/badge/React-%2361DAFB.svg?style=for-the-badge&logo=react&logoColor=black"
-  },
-  {
-    name: "HTML5",
-    logo: "https://img.shields.io/badge/HTML5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
-  },
-  {
-    name: "CSS3",
-    logo: "https://img.shields.io/badge/CSS3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
-  },
-  {
-    name: "TypeScript",
-    logo: "https://img.shields.io/badge/TypeScript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
-  },
-  {
-    name: "Next.js",
-    logo: "https://img.shields.io/badge/Next.js-%23000000.svg?style=for-the-badge&logo=next.js&logoColor=white"
-  },
-  {
-    name: "Angular",
-    logo: "https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white"
-  },
-  {
-    name: "Vue.js",
-    logo: "https://img.shields.io/badge/Vue.js-%234FC08D.svg?style=for-the-badge&logo=vue.js&logoColor=white"
-  },
-  {
-    name: "React Native",
-    logo: "https://img.shields.io/badge/React_Native-%2361DAFB.svg?style=for-the-badge&logo=react&logoColor=black"
-  },
-  {
-    name: "Expo",
-    logo: "https://img.shields.io/badge/Expo-000020.svg?style=for-the-badge&logo=expo&logoColor=white"
-  },
-  {
-    name: "Tailwind CSS",
-    logo: "https://img.shields.io/badge/TailwindCSS-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
-  },
-  {
-    name: "Astro",
-    logo: "https://img.shields.io/badge/Astro-%23FF5D01.svg?style=for-the-badge&logo=astro&logoColor=white"
-  },
-  {
-    name: "SASS",
-    logo: "https://img.shields.io/badge/SASS-%23CC6699.svg?style=for-the-badge&logo=sass&logoColor=white"
-  },
-  {
-    name: "Bootstrap",
-    logo: "https://img.shields.io/badge/Bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white"
-  },
-  {
-    name: "Figma",
-    logo: "https://img.shields.io/badge/Figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white"
-  },
-  {
-    name: "Git",
-    logo: "https://img.shields.io/badge/Git-%23F05032.svg?style=for-the-badge&logo=git&logoColor=white"
-  },
-  {
-    name: "Redux",
-    logo: "https://img.shields.io/badge/Redux-%23764ABC.svg?style=for-the-badge&logo=redux&logoColor=white"
-  },
-  {
-    name: "Firebase",
-    logo: "https://img.shields.io/badge/Firebase-orange.svg?style=for-the-badge&logo=firebase&logoColor=white"
-  },
-  {
-    name: "Jest",
-    logo: "https://img.shields.io/badge/Jest-C21325.svg?style=for-the-badge&logo=jest&logoColor=white"
-  },
-  {
-    name: "Node.js",
-    logo: "https://img.shields.io/badge/Node.js-%23339933.svg?style=for-the-badge&logo=nodedotjs&logoColor=white"
-  },
-  {
-    name: "Express",
-    logo: "https://img.shields.io/badge/Express-%23000000.svg?style=for-the-badge&logo=express&logoColor=white"
-  },
-  {
-    name: "MongoDB",
-    logo: "https://img.shields.io/badge/MongoDB-%2347A248.svg?style=for-the-badge&logo=mongodb&logoColor=white"
-  },
-];
+export const skills = {
+  languages: [
+    "JavaScript",
+    "TypeScript",
+    "HTML5",
+    "CSS3",
+    "SASS"
+  ],
+  frameworks: [
+    "React",
+    "Next.js",
+    "Vue.js",
+    "Angular",
+    "Astro",
+    "React Native",
+    "Expo"
+  ],
+  styling: [
+    "Tailwind CSS",
+    "Bootstrap",
+    "shadcn",
+    "CSS Animations"
+  ],
+  backend: [
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Firebase",
+    "Appwrite"
+  ],
+  tools: [
+    "Git",
+    "Figma",
+    "Jest",
+    "Redux",
+    "Tanstack Query",
+    "Storybook",
+    "Postman"
+  ]
+};
