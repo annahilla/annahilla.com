771155c212d31ea612d81fd013ca9534e9cb1221
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-20 13:04:42 +0200

    Add projects section

---
+++ src/constants/projects.ts
@@ -0,0 +1,24 @@
+import project1 from "../assets/projects/project1.png";
+import project2 from "../assets/projects/project2.png";
+import project3 from "../assets/projects/project3.png";
+
+export const projects = [
+    {
+        id: 1,
+        name: "Nissu",
+        src: project1.src,
+        link: "https://nissu.app/"
+    },
+    {
+        id: 2,
+        name: "Planutri",
+        src: project2.src,
+        link: "https://planutri.vercel.app/"
+    },
+    {
+        id: 3,
+        name: "Wellpulse",
+        src: project3.src,
+        link: "https://wellpulse.vercel.app/"
+    },
+]
