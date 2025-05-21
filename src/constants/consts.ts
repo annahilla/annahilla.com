fb566ef44ca8a28ec15470f1fd0ac556ce3d6d77
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-21 13:57:50 +0200

    Improve seo

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,5 +1,12 @@
+export const title = "Anna Hilla · Frontend Web Developer";
+export const description =
+  "Hi, I'm Anna Hilla – a frontend developer who loves crafting clean, responsive, and user-friendly web and mobile apps with React, Next.js, React Native and a modern tech stack.";
+export const previewImage = "https://annahilla.com/preview.png";
+export const url = "https://annahilla.com";
+
 export const githubLink = "https://github.com/annahilla";
 export const linkedinLink = "https://www.linkedin.com/in/anna-hilla";
-export const mail = "annahilla21@gmail.com"
+export const mail = "annahilla21@gmail.com";
+
 
 export const buttonStyles = "text-center uppercase border py-2 w-46 font-light tracking-[0.15rem]"
