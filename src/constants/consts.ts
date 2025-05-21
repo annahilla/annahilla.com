f706296e9a5383ccea366df38f0072314832c8d2
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-21 11:57:00 +0200

    Add circle pointer

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -2,4 +2,4 @@ export const githubLink = "https://github.com/annahilla";
 export const linkedinLink = "https://www.linkedin.com/in/anna-hilla";
 export const mail = "annahilla21@gmail.com"
 
-export const buttonStyles = "text-center uppercase border py-2 w-46 font-light tracking-[0.15rem] cursor-pointer hover:opacity-70"
+export const buttonStyles = "text-center uppercase border py-2 w-46 font-light tracking-[0.15rem]"
